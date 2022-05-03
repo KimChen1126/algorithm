9b2e88007dad3b4163cf0b36b358b43cf22af268
unknown <107022210@office365.nthu.edu.tw>
2022-05-04 00:48:32 +0800

    brute force implementation complete, pending time analysis and branch and bound method

--- hw4/hw4_bnb.cpp
+++ hw4/hw4_bnb.cpp
@@ -2,29 +2,84 @@
 #include<bits/stdc++.h>
 using namespace std;
 using namespace std::chrono;
-auto time()
+// bool compare(const pair<pair<float,float>,float>&i, const pair<pair<float,float>,float>&j) 
+// { 
+//     return i.second < j.second; 
+// } 
+bool sortbyfir(const pair<float,pair<float,float>> &a,
+                   const pair<float,pair<float,float>> &b)
 {
-    
-    auto start = high_resolution_clock::now();
-    // Get ending timepoint
-    auto stop = high_resolution_clock::now();
- 
-    // Get duration. Substart timepoints to
-    // get duration. To cast it to proper unit
-    // use duration cast method
-    auto duration = duration_cast<microseconds>(stop - start);
- 
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
+       return a.first>b.first;
+}
+auto bruteforce()
+{
+
+    vector<pair<float,pair<float,float>>>item;
+    int n;
+    float capacity;
+    cin>>n>>capacity;
+    for(auto i=0;i<n;i++)
+    {
+        // int price,capa;
+        float cost;
+        // pair<float,float>tmp;
+        pair<float,pair<float,float>>tmpp;
+        cin>>tmpp.second.first>>tmpp.second.second;
+        cost=(tmpp.second.first/tmpp.second.second);
+        tmpp.first=cost;
+        item.push_back(tmpp);
+    }
+    cout<<"test"<<endl;
+    for(auto j=0;j<n;j++)
+    {
+        cout<<fixed<<setprecision(3);
+        cout<<"cost="<<item[j].first<<" price="<<item[j].second.first<<" weight="<<item[j].second.second<<endl;
+    }
+    sort(item.begin(),item.end(),sortbyfir);
+    cout<<"after sorting:"<<endl;
+    for(auto j=0;j<n;j++)
+    {
+        cout<<fixed<<setprecision(3);
+        cout<<"cost="<<item[j].first<<" price="<<item[j].second.first<<" weight="<<item[j].second.second<<endl;
+    }
+    float ans=0;
+    for(auto &k:item)
+    {
+        if(k.second.second<=capacity)
+        {
+            capacity-=k.second.second;
+            ans+=k.second.first;
+        }
+        else break;
+    }
+
+    cout<<"ans="<<ans<<endl;
+}
+/*
+P W
+10 269
+55 95
+10 4
+47 60
+5 32
+4 23
+50 72 
+8 80 
+61 62 
+85 65
+87 46
+*/
+auto branchandbound(){
+/*
+please implement you code here, gg:)
+*/
 }
 int main()
 {
+
     auto start = high_resolution_clock::now();
-    cout<<"hello world"<<endl;
-    string s;
-    s="fuck";
-    cout<<s<<endl;
-    // time();
+    bruteforce();
+    // branchandbound();
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: "
