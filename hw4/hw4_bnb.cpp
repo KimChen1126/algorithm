6ccee2ad17b0b4c848b86b97936d83d73b3669aa
unknown <107022210@office365.nthu.edu.tw>
2022-05-05 10:11:15 +0800

    Testcases branch and bound plotting.py updated

--- hw4/hw4_bnb.cpp
+++ hw4/hw4_bnb.cpp
@@ -3,14 +3,37 @@
 using namespace std;
 using namespace std::chrono;
 int n;
+struct Node
+{
+    int lv,profit,bound;
+    float weight;
+};
 bool sortbyfir(const pair<float,pair<float,float>> &a,
                    const pair<float,pair<float,float>> &b)
 {
        return a.first>b.first;
 }
-auto bruteforce()
+int bound(Node u, int n,float capacity,vector<pair<float,pair<float,float>>>&item)
 {
-    vector<pair<float,pair<float,float>>>item;
+    if(u.weight>=capacity)return 0;
+    int probound=u.profit;
+    int j=u.lv+1;
+    int total=u.weight;
+    while((j<n)&&(total+item[j].second.second<=capacity))
+    {
+        total+=item[j].second.second;
+        probound+=item[j].second.first;
+        j++;
+    }
+    if(j<n)
+    {
+        probound+=(capacity-total)*item[j].first;
+    }
+    return probound;
+}
+auto branchandbound()
+{
+    vector<pair<float,pair<float,float>>>item;//item cost value, weight 
     float capacity;
     cin>>n>>capacity;
     
@@ -38,15 +61,31 @@ auto bruteforce()
     //     cout<<fixed<<setprecision(3);
     //     cout<<"cost="<<item[j].first<<" price="<<item[j].second.first<<" weight="<<item[j].second.second<<endl;
     // }
-    float ans=0;
-    for(auto &k:item)
+    queue<Node>Q;
+    Node u,v;
+    u.lv=-1;
+    u.profit=u.weight=0;
+    Q.push(u);
+    int ans=0;
+    while(!Q.empty())
     {
-        if(k.second.second<=capacity)
-        {
-            capacity-=k.second.second;
-            ans+=k.second.first;
-        }
-        else break;
+        u=Q.front();
+        Q.pop();
+        if(u.lv==-1)v.lv=0;
+        if(u.lv==n-1)continue;
+        v.lv=u.lv+1;
+        v.weight=u.weight+item[v.lv].second.second;
+        v.profit=u.profit+item[v.lv].second.first;
+
+        if(v.weight<=capacity&&v.profit>ans)ans=v.profit;
+        v.bound=bound(v,n,capacity,item);
+        if (v.bound>ans)Q.push(v);
+
+        v.weight=u.weight;
+        v.profit=u.profit;
+        v.bound=bound(v,n,capacity,item);
+        if(v.bound>ans)
+        Q.push(v);
     }
 
     cout<<"ans="<<ans<<endl;
@@ -73,14 +112,25 @@ P W
 85 65
 87 46
 */
-auto branchandbound(){
+auto bruteforce(){
 /*
-please implement you code here, gg:)
+ you code here, gg:)
+*/
+
+/* time function
+auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << float(duration.count()) /1000000<< " seconds" << endl;
+    ofstream out;
+    out.open("./result2.txt",ios::app);
+    out<<n<<" "<<float(duration.count()) /1000000<<endl;
+    return 0;
 */
 }
 int main()
 {  
-    bruteforce();
-    // branchandbound();   
+    //bruteforce();
+    branchandbound();   
     return 0;
 }
--- plotresult.py
+++ plotresult.py
@@ -1,21 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os,math
-n=[]
-time=[]
+row=41
+time=[[]]*8
+aver=[]
+ans=[]
+n=[5,10,15,20,25,30,35,40]
 os.chdir('D:/algorithm/hw4')
 with open("./result.txt",'r')as f:
+    tmpp=0
     lines=f.readlines()
+    sum=0
     for line in lines:
         tmp=line.split()
-        n.append(tmp[0])
-        time.append((tmp[1]) )
-    for val in n :
-        print(val)
-    for vall in time:
-        print(vall)
+        sum+=float(tmp[1])
+        tmpp+=1
+        if tmpp==3:
+            tmpp=0
+            aver.append(sum)
+            sum=0
+for val in n :
+    print(val)
+# for i in range(len(time)):
+#     aver.append(np.log2(sum(time[i])/len(time[i])))
+for ave in aver:
+    print(ave)
+    ans.append(np.log(ave))
+    print(ave)
 plt.xlabel('# of items')
 plt.ylabel('Time Complexity')
-plt.plot(n,time, label ='bruteforce')
+plt.plot(n,ans, label ='bruteforce')
 plt.grid()
 plt.show()
