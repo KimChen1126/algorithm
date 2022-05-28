6571cab5acffaf9cc8e087c8ba9f96d46de846c2
unknown <107022210@office365.nthu.edu.tw>
2022-05-28 21:44:43 +0800

    hw done, pending to produce random testcases to run plot

--- hw5/hw5_pns.cpp
+++ hw5/hw5_pns.cpp
@@ -12,22 +12,85 @@ using namespace std::chrono;
 19
 26
 */
-void pruneandsearch()
+int findmedian(vector<int>&,int,int);
+void mysort(vector<int>&arr,int left,int right)
 {
-    // auto start = high_resolution_clock::now();
-
-
-    // auto stop = high_resolution_clock::now();
-    // auto duration = duration_cast<microseconds>(stop - start);
-    // cout << "Time taken by function: "
-    //      << float(duration.count()) /1000000<< " seconds" << endl;
-    // ofstream out;
-    // out.open("./result.txt",ios::app);
-    // out<<n<<" "<<float(duration.count()) /1000000<<endl;
+    int i,j,v;
+    for(i = left+1; i <= right; i++) {
+            v = arr[i];
+            j = i;
+            while(j-1 >= left && arr[j-1] > v)
+            {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = v;
+    }
+}
+int pruneandsearch(vector<int>&arr,int left,int right, int target)
+{      
+    if(right-left+1 <= 5) {
+        int i, j, v;
+        mysort(arr,left,right);
+        return left+target;
+    }   
+    int pivot = findmedian(arr, left,right);
+    swap(arr[left], arr[pivot]);
+    int i, j;
+    int t = arr[left];
+    for(i = left, j = left+1; j <= right; j++) {
+        if(arr[j] <= t)
+        {           
+            i++;
+            swap(arr[i], arr[j]);
+            
+        }    
+    }
+    swap(arr[left], arr[i]);
+    // partition end.
+    int position = i;
+    if(position-left == target)    
+        return position;
+    if(position-left < target)
+        return pruneandsearch(arr, position+1, right, target-(position-left+1));
+    else
+        return pruneandsearch(arr, left, position, target);
+}
+int findmedian(vector<int>&arr, int l, int r) {
+    int med = (r-l+4)/5;
+    int i, subleft, subright;
+    int medIdx;
+    for(i = 0; i < med; i++) {
+        subleft = l + i*5;
+        subright = subleft + 4;
+        if(subright > r)    
+            subright = r;
+        medIdx = pruneandsearch(arr, subleft, subright, (subright-subleft)/2);
+        swap(arr[l+i], arr[medIdx]);
+    }
+    
+    return pruneandsearch(arr, l, l+med, med/2);
 }
 int main()
 {
-    
-    // pruneandsearch();
+    vector<int>arr;
+    int n,k,tmp;
+    cin>>n>>k;
+    // cout<<n<<k;
+    for(auto i=0;i<n;i++)
+    {   
+        cin>>tmp;
+        arr.push_back(tmp);
+    }
+    auto start = high_resolution_clock::now();
+    int idx=pruneandsearch(arr,0,n-1,k-1);
+    cout<<arr[idx]<<endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "
+         << float(duration.count()) /1000000<< " seconds" << endl;
+    ofstream out;
+    out.open("./result.txt",ios::app);
+    out<<n<<" "<<float(duration.count()) /1000000<<endl;
     return 0;
 }
---
+++ hw5/plotresult.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os,math
+aver=[]
+ans=[]
+brute=[]
+n=[5,10,15,20,25,30,35,40]
+os.chdir('D:/algorithm/hw4')
+print(os.environ['USERPROFILE'])
+
+with open("./result.txt",'r')as f:
+    tmpp=0
+    lines=f.readlines()
+    sum=0
+    for line in lines:
+        tmp=line.split()
+        sum+=float(tmp[1])
+        tmpp+=1
+        if tmpp==3:
+            tmpp=0
+            aver.append(sum/3)
+            sum=0
+with open("./result2.txt",'r')as g:
+    lines=g.readlines()
+    for line in lines:
+        print(line)
+        tmp=line.split()
+        print(tmp)
+        brute.append(float(tmp[1]))
+
+for ave in aver:
+    print(ave)
+    ans.append(ave)
+    # print(ave)
+print(brute)
+plt.title("[107022210/107022218]brute force vs branch and bound ")
+plt.xlabel('# of items')
+plt.ylabel('Time(sec)')
+plt.plot(n,ans)
+plt.plot(n,brute)
+plt.legend(["branch and bound","brute force"])
+plt.yscale("log")
+plt.grid()
+plt.show()
+sys.exit()
